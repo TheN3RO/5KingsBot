ba493876db563ffdde4d2618b382a7406c4d7584
robipl12 <58180782+robipl12@users.noreply.github.com>
2020-12-17 14:31:11 +0100

    Update poll.command.js

--- src/commands/poll.command.js
+++ src/commands/poll.command.js
@@ -10,7 +10,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             .setTitle("Aplikacja do 5Kings Academy.")
             .setThumbnail("https://cdn.discordapp.com/attachments/761836839195246632/788323549785882654/5kings_logo.png")
-            .setDescription("Informujemy że jutro(18.12.2020) o godzinie 19:00 odbędzie się rekrutacja do pierwszego składu 5Kings Academy. Czekamy!")
+            .setDescription("Informujemy że jutro(18.12.2020) o godzinie 19:00 odbędzie się rekrutacja do pierwszego składu 5Kings Academy. Good luck!")
             .setColor("FFBF00")
         msg.channel.send(embed)
     }
