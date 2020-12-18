47849b348d37c45f38475b567bf6281b49542c92
robipl12 <58180782+robipl12@users.noreply.github.com>
2020-12-18 14:02:54 +0100

    Update approved.command.js

--- src/commands/approved.command.js
+++ src/commands/approved.command.js
@@ -7,50 +7,51 @@ module.exports = {
     run(msg, args) {
         const { member, channel } = msg
         if (member.permissionsIn(channel).has(["ADMINISTRATOR"])) {}else {return msg.reply("Nie masz permisji do tej koemndy!!")}
-        const collector = new Discord.MessageCollector(msg.channel, m => m.author.id === msg.author.id, { time: 10000 })
-        msg.channel.send("Odrzucone/Przyjęte|masz 10s na odp")
-        collector.on("collect", msg => {
-            let title = msg.content || args[0]
-            collector.on("end", collected => {
-                msg.channel.send("Nick gracza: |masz 15s na odp")
-                const collector = new Discord.MessageCollector(msg.channel, m => m.author.id === msg.author.id, { time: 15000 })
-                collector.on("collect", msg => {
-                    let nick = msg.content || args[1]
-                    collector.on("end", collected => {
-                        if (title.toUpperCase() === "PRZYJĘTE") {
-                            console.log("przyjęte")
-                            const channelprzyjete = msg.guild.channels.cache.find(i => i.name === "⌊✅⌉𝘈𝘱𝘱𝘳𝘰𝘷𝘦𝘥")
-                            const przyjete = new Discord.MessageEmbed()
-                            .setTitle(`Podanie zostało ${title.toUpperCase()}`)
-                            .setThumbnail("https://i.imgur.com/NV5yoAw.png")
-                            .setColor("30fc03")
-                            .setDescription(`
-                            Użytkowniku: @${nick}!
-                            Twoja aplikacja na: **5-Kings Academy** została rozpatrzona **pozytywnie**!
-                            `)
-                            .addField("――――――――――――――――――――――――――", "――――――――――――――――――――――――――")
-                            .setTimestamp()
-                            .setFooter(`Aplikacje sprawdził administrator: ${member.user.tag}`, member.user.displayAvatarURL({ format: "png" }))
-                            channelprzyjete.send(przyjete)
-                        }else if (title.toUpperCase() === "ODRZUCONE") {
-                            console.log("odrzucone")
-                            const channelodrzucone = msg.guild.channels.cache.find(i => i.name === "⌊❌⌉𝘙𝘦𝘫𝘦𝘤𝘵")
-                            const odrzucone = new Discord.MessageEmbed()
-                            .setTitle(`Podanie zostało ${title.toUpperCase()}`)
-                            .setThumbnail("https://i.imgur.com/S6CSLLF.png")
-                            .setColor("FF0000")
-                            .setDescription(`
-                            Użytkowniku: @${nick}!
-                            Twoja aplikacja na: **5-Kings Academy** została rozpatrzona **negatywnie**!
-                            `)
-                            .addField("――――――――――――――――――――――――――", "――――――――――――――――――――――――――")
-                            .setTimestamp()
-                            .setFooter(`Aplikacje sprawdził administrator: ${member.user.tag}`, member.user.displayAvatarURL({ format: "png" }))
-                            channelodrzucone.send(odrzucone)
-                        }
-                    })
-                })
-            })    
-        })
+        const niepoprawne1 = new Discord.MessageEmbed()
+            .setTitle("Error")
+            .setColor("ff0000")
+            .setDescription("Niepoprawny nick gracza! Sprubój: !aplications Przyjęte/Odrzucone _nick-gracza_")
+        const niepoprawne2 = new Discord.MessageEmbed()
+            .setTitle("Error")
+            .setColor("ff0000")
+            .setDescription("Niepoprawny tytuł! Sprubój: !aplications Przyjęte/Odrzucone _nick-gracza_")
+        let title = args[0]
+        let nick = args[1]
+        if (!nick) {
+            msg.channel.send(niepoprawne1)
+        }
+        if (title.toUpperCase() === "PRZYJĘTE") {
+            console.log("przyjęte")
+            const channelprzyjete = msg.guild.channels.cache.find(i => i.name === "⌊✅⌉𝘈𝘱𝘱𝘳𝘰𝘷𝘦𝘥")
+            const przyjete = new Discord.MessageEmbed()
+                .setTitle(`Podanie zostało ${title.toUpperCase()}`)
+                .setThumbnail("https://i.imgur.com/NV5yoAw.png")
+                .setColor("30fc03")
+                .setDescription(`
+                Użytkowniku: @${nick}!
+                Twoja aplikacja na: **5-Kings Academy** została rozpatrzona **pozytywnie**!
+                `)
+                .addField("――――――――――――――――――――――――――", "――――――――――――――――――――――――――")
+                .setTimestamp()
+                .setFooter(`Aplikacje sprawdził administrator: ${member.user.tag}`, member.user.displayAvatarURL({ format: "png" }))
+                channelprzyjete.send(przyjete)
+        }else if (title.toUpperCase() === "ODRZUCONE") {
+            console.log("odrzucone")
+            const channelodrzucone = msg.guild.channels.cache.find(i => i.name === "⌊❌⌉𝘙𝘦𝘫𝘦𝘤𝘵")
+            const odrzucone = new Discord.MessageEmbed()
+                .setTitle(`Podanie zostało ${title.toUpperCase()}`)
+                .setThumbnail("https://i.imgur.com/S6CSLLF.png")
+                .setColor("FF0000")
+                .setDescription(`
+                Użytkowniku: @${nick}!
+                Twoja aplikacja na: **5-Kings Academy** została rozpatrzona **negatywnie**!
+                `)
+                .addField("――――――――――――――――――――――――――", "――――――――――――――――――――――――――")
+                .setTimestamp()
+                .setFooter(`Aplikacje sprawdził administrator: ${member.user.tag}`, member.user.displayAvatarURL({ format: "png" }))
+                channelodrzucone.send(odrzucone)
+        } else {
+            msg.channel.send(niepoprawne2)
+        } 
     }
 }
